c64be9a2c400a984b4dfefd0b9724724def9873b
rk1024 <rookie1286@gmail.com>
2015-07-15 13:49:00 -0400

    Scaffolding Menu class

---
+++ RogueClicker/engine/menu.py
@@ -0,0 +1,10 @@
+from engine.drawable import *
+
+class Menu(Drawable):
+  def __init__(self, surf):
+    Drawable.__init__(self, surf)
+
+    #TODO: Add construction code here
+
+  def draw(self, dt):
+    pass #Add drawing code here (dt is delta-time, surf is your surface)
