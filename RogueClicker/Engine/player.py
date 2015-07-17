05986e45f51bc0ba6027ee1c3bdc75e38b3ece7d
zwil97 <zwilson@ameritech.net>
2015-07-17 10:33:01 -0400

    Player == Player

--- RogueClicker/Engine/player.py
+++ RogueClicker/Engine/player.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 class Player(Mob):
   def __init__(self, pos, *groups):
-    Mob.__init__(self, pos, pg.image.load("assets\\sprites\\test.png"), 4, *groups)
+    Mob.__init__(self, pos, pg.image.load("assets\\sprites\\Player.png"), 4, *groups)
 
   def update(self, dt):
     keys = pg.key.get_pressed()
