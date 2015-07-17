0e7a7176559710ef2bee45430b29181775f7afec
rk1024 <rookie1286@gmail.com>
2015-07-17 12:56:45 -0400

    Fizacks

--- RogueClicker/Engine/mob.py
+++ RogueClicker/Engine/mob.py
@@ -1,7 +1,7 @@
 ﻿import numpy as np
 from engine.entity import *
 
-class Mob(Entity):
+class Mob(DynEntity):
   def __init__(self, world, pos, vel, surf, scl, *groups):
     Entity.__init__(self, world, pos, vel, surf, scl, *groups)
 
@@ -11,10 +11,6 @@ class Mob(Entity):
     self.isJumping = False
 
   def update(self, dt):
-    
-    self.vel[0] = self.walkDir
-    self.vel[1] = -1 if self.isJumping else 0
+    self.vel += np.array([self.walkDir, -1 if self.isJumping else 0]) * .1
 
-    self.vel *= .5
-
-    Entity.update(self, dt)
+    DynEntity.update(self, dt)
