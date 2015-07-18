a3479867c0aeb2f6c49fdf3b83de5ab41de3fd3c
rk1024 <rookie1286@gmail.com>
2015-07-18 10:45:13 -0400

    Here you go, Zac.

--- RogueClicker/Engine/player.py
+++ RogueClicker/Engine/player.py
@@ -7,10 +7,13 @@ class Player(Mob):
     self.defImg = pg.image.load("assets\\sprites\\player.png")
     self.jumpImg = pg.image.load("assets\\sprites\\playerJumping.png")
     self.crouchImg = pg.image.load("assets\\sprites\\playerCrouch.png")
-    self.walkAll = pg.image.load("assets\\sprites\\playerWalk.png")
-    self.walkFrame = []
-    self.i = 0
+
     Mob.__init__(self, world, pos, vel, self.defImg, 1, *groups)
+
+    self.walkFrame = []
+    self.loadWalk()
+    self.i = 0
+
     self.initPos = self.pos.copy()
     self.initVel = self.vel.copy()
     self.inAir = False
@@ -23,7 +26,7 @@ class Player(Mob):
 
     self.walkDir = keys[pg.K_d] - keys[pg.K_a]
 
-    if self.walkDir != 0: self.image = self.walkFrame[self.i]
+    if self.walkDir != 0: self.image = self.walkFrame[self.i] # LIST OUT OF RANGE ERROR - HAVEN'T FIXED YET
 
     self.isJumping = keys[pg.K_SPACE]
     if self.isJumping: self.inAir = True
@@ -39,4 +42,7 @@ class Player(Mob):
       self.pos = self.initPos.copy()
       self.vel = self.initVel.copy()
 
-  def loadWalk(self, image, sliceSize):
+  def loadWalk(self):
+    for i in range(8):
+      self.walkFrame.append(pg.image.load("assets\\sprites\\walkFrames\\Player_F" + str(i) + ".png"))
+    
