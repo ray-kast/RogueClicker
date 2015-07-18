55c813a0d5d3c21be27456b185d55d6544fb1452
zwil97 <zwilson@ameritech.net>
2015-07-18 18:58:36 -0400

    Jump, run, shoot sprites for facing left and right.

--- RogueClicker/Engine/player.py
+++ RogueClicker/Engine/player.py
@@ -11,30 +11,40 @@ class Player(Mob):
     Mob.__init__(self, world, pos, vel, self.defImg, 1, *groups)
 
     self.walkFrame = []
+    self.shootFrame = []
     self.loadWalk()
-    self.i = 0
+    self.loadShoot()
+    self.walk_f = 0
 
     self.initPos = self.pos.copy()
     self.initVel = self.vel.copy()
     self.inAir = False
 
   def update(self, dt):
-    if self.i >= 8: self.i = 0
+    if self.walk_f >= 8: self.walk_f = 0
 
     keys = pg.key.get_pressed()
 
     self.walkDir = keys[pg.K_d] - keys[pg.K_a]
 
-    self.isJumping = keys[pg.K_SPACE]
+    self.isJumping = keys[pg.K_SPACE] or keys[pg.K_w]
     self.image = self.defImg if not self.isJumping else self.jumpImg 
     
     if self.isJumping: self.inAir = True
     if self.isOnGround: self.inAir = False
     
     if self.walkDir > 0:
-      self.image = self.walkFrame[self.i]
-    
+      self.image = self.walkFrame[self.walk_f]
+    if self.walkDir < 0:
+      self.image = self.walkFrame[self.walk_f]
+      self.image = pg.transform.flip(self.image, True, False)
+
     if self.inAir: self.image = self.crouchImg
+
+    if keys[pg.K_v]:
+      self.walkDir = 0
+      self.shoot()
+      if keys[pg.K_a]: self.image = pg.transform.flip(self.image, True, False)
 
     Mob.update(self, dt)
 
@@ -42,8 +52,20 @@ class Player(Mob):
       self.pos = self.initPos.copy()
       self.vel = self.initVel.copy()
 
-    self.i += 1
+    self.walk_f += 1
+
   def loadWalk(self):
     for i in range(8):
       self.walkFrame.append(pg.image.load("assets\\sprites\\walkFrames\\Player_F" + str(i) + ".png"))
+
+  def loadShoot(self):
+    for n in range(3):
+      self.shootFrame.append(pg.image.load("assets\\sprites\\shootFrames\\playerPistol_f" + str(n) + ".png"))
+
+  def shoot(self):
+    if self.image != self.shootFrame[2]:
+      self.image = self.shootFrame[0]
+      self.image = self.shootFrame[1]
+      self.image = self.shootFrame[2]
+    else: self.image = self.shootFrame[2]
     
