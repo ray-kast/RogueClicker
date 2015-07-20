4a0909fe0fc5077c00991fdebc0de48712b17f8c
rk1024 <rookie1286@gmail.com>
2015-07-20 14:22:45 -0400

    Physics engine tweaks

--- RogueClicker/RogueClicker.py
+++ RogueClicker/RogueClicker.py
@@ -5,7 +5,6 @@ try:
   game = TestGame()
 
   game.run()
-
 except BaseException as e:
   print("Caught {0}: {1}\n{2}".format(type(e).__name__, e, "".join(traceback.format_tb(e.__traceback__))))
 
--- RogueClicker/engine/world.py
+++ RogueClicker/engine/world.py
@@ -30,28 +30,27 @@ class World(Drawable):
           spriteCollides = True
           if sprite.lastRect.right <= ent.rect.left \
             and sprite.rect.right > ent.rect.left:
-            if sprite.vel[0] > 0: sprite.vel[0] *= -.2
+            if sprite.vel[0] > 0: sprite.vel[0] *= ent.bounce[0]
             sprite.pos[0] += ent.rect.left - sprite.rect.right
 
           if sprite.lastRect.left >= ent.rect.right \
             and sprite.rect.left < ent.rect.right:
-            if sprite.vel[0] < 0: sprite.vel[0] *= -.2
+            if sprite.vel[0] < 0: sprite.vel[0] *= ent.bounce[2]
             sprite.pos[0] += ent.rect.right - sprite.rect.left
 
           if sprite.lastRect.bottom <= ent.rect.top \
             and sprite.rect.bottom > ent.rect.top:
-            if sprite.vel[1] > 0: sprite.vel[1] *= -.2
+            if sprite.vel[1] > 0: sprite.vel[1] *= ent.bounce[1]
             sprite.pos[1] += ent.rect.top - sprite.rect.bottom
             sprite.isOnGround = True
 
           if sprite.lastRect.top >= ent.rect.bottom \
             and sprite.rect.top < ent.rect.bottom:
-            if sprite.vel[1] < 0: sprite.vel[1] *= -.2
+            if sprite.vel[1] < 0: sprite.vel[1] *= ent.bounce[3]
             sprite.pos[1] += ent.rect.bottom - sprite.rect.top
       
       if not spriteCollides:
         sprite.lastRect = sprite.rect
-
 
     self.bkgdSprites.draw(self.surf)
     self.envSprites.draw(self.surf)
