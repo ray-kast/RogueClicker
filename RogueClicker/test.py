752f956084ac53b6fd183f2978c5da8b123ae644
rk1024 <rookie1286@gmail.com>
2015-07-20 09:53:56 -0400

    Skinned the level

--- RogueClicker/test.py
+++ RogueClicker/test.py
@@ -9,14 +9,21 @@ class TestGame(Game):
     Game.__init__(self)
 
   def doBlocks(self, blocks, size):
-    surf = pg.Surface((size, size))
+    surf = pg.transform.scale(pg.image.load("assets\\sprites\\blocks\\metalBlock32x32.png"), (size, size))
+    shSurf = pg.Surface((size, size))
 
-    surf.fill(Colors.Black)
+    shSurf.fill(Colors.Black)
+    shSurf.set_alpha(int(255 * .15))
     
     ents = []
+    shEnts = []
     
     for block in blocks:
-      ents.append(Entity(self.world, np.multiply(block, size), (0, 0), surf, 1, self.world.envSprites))
+      pos = np.multiply(block, size)
+      ents.append(Entity(self.world, pos, (0, 0), surf, 1, self.world.envSprites))
+      shEnts.append(Entity(self.world, np.add(pos, (8, 8)), (0, 0), shSurf, 1))
+
+    self.world.bkgdSprites.add(*shEnts, layer = 1)
 
   def init(self):
     Game.init(self)
@@ -71,9 +78,20 @@ class TestGame(Game):
 
     self.player = Player(self.world, (110, 110), (0, 0), self.world.dynSprites)
 
-    surf = pg.Surface((100, 100))
+    surf = pg.Surface(self.scrRect.size)
 
-    surf.fill(Colors.Black)
+    img = pg.image.load("assets\\sprites\\blocks\\metalSheet32x32.png")
+    size = np.multiply(img.get_rect().size, 2)
+
+    img = pg.transform.scale(img, size)
+
+    for row in range(0, self.scrRect.height, size[1]):
+      for col in range(0, self.scrRect.width, size[0]):
+        surf.blit(img, (col, row))
+
+    self.bkgd = Entity(self.world, (0, 0), (0, 0), surf, 1)
+
+    self.world.bkgdSprites.add(self.bkgd, layer = 0)
 
     self.blocks = self.doBlocks([
       (1, 4),
@@ -84,15 +102,12 @@ class TestGame(Game):
       (6, 4),
       (7, 4),
       (8, 4),
-      (7, 4),
-      (8, 4),
       (1, 2),
       (2, 2),
       (3, 2),
       (4, 2),
       (5, 2),
       (6, 2),
-      (1, 2)
     ],64 )
 
     self.currDrawing = self.menu
